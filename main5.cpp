bb6c1c4f1d9fcb9fdfc41165e3ce11b715611ebd
atamofidi <151543259+atamofidi@users.noreply.github.com>
2023-11-24 09:48:05 -0800

    Add files via upload

---
+++ main5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+ main() {
+	float b , d , a , s, x, y;  
+	
+	
+	 
+	
+	cin >> x >> y;
+	s=x+y;
+	
+	a=x-y;  
+	
+	b=x*y;
+	
+	d=x/y;
+	
+   cout<<s;	
+   
+   
+   
+   cout<<a;	
+   
+   
+   cout<<b;	
+   
+   
+   cout<<d;	
+   
+}
